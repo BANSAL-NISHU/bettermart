f4270b7bd3decc8fd4ed1168acdae9cbb8bf5af4
BANSAL-NISHU <bansalnishu418@gmail.com>
2023-05-25 15:51:10 +0530

    Updated LoginScreen

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -6,6 +6,7 @@ import Message from "../components/Message";
 import Loader from "../components/Loader";
 import FormContainer from "../components/FormContainer";
 import { login } from "../actions/userActions";
+import secure_login from "../images/secure_login.svg";
 
 const LoginScreen = ({ location, history }) => {
   const [email, setEmail] = useState("");
@@ -30,45 +31,59 @@ const LoginScreen = ({ location, history }) => {
   };
 
   return (
-    <FormContainer>
-      <h1>Sign In</h1>
-      {error && <Message variant="danger">{error}</Message>}
-      {loading && <Loader />}
-      <Form onSubmit={submitHandler}>
-        <Form.Group controlId="email">
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+    <div>
+      <div className="split right">
+        <FormContainer>
+          <h1>Sign In</h1>
+          {error && <Message variant="danger">{error}</Message>}
+          {loading && <Loader />}
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId="email">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                id="form_control"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
 
-        <Form.Group controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter password"
+                id="form_control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
 
-        <Button type="submit" variant="primary">
-          Sign In
-        </Button>
-      </Form>
+            <Button type="submit" variant="primary">
+              Sign In
+            </Button>
+          </Form>
 
-      <Row className="py-3">
-        <Col>
-          New Customer?{" "}
-          <Link to={redirect ? `/register?redirect=${redirect}` : "/register"}>
-            Register
-          </Link>
-        </Col>
-      </Row>
-    </FormContainer>
+          <Row className="py-3">
+            <Col>
+              New Customer?{" "}
+              <Link
+                to={redirect ? `/register?redirect=${redirect}` : "/register"}
+              >
+                Register
+              </Link>
+            </Col>
+          </Row>
+        </FormContainer>
+      </div>
+
+      <div className="split left">
+        <figure>
+          <img className="secure-login" src={secure_login} alt="Sign In" />
+        </figure>
+      </div>
+    </div>
   );
 };
 
