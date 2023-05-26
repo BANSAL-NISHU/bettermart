73b58d2b9f0b263d2aa07c18584a33d18ea83a27
BANSAL-NISHU <bansalnishu418@gmail.com>
2023-05-26 14:56:51 +0530

    added more payment options + design changes

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
 import CheckoutSteps from "../components/CheckoutSteps";
 import { savePaymentMethod } from "../actions/cartActions";
+import payment from "../images/payment.svg";
 
 const PaymentScreen = ({ history }) => {
   const cart = useSelector((state) => state.cart);
@@ -13,7 +14,7 @@ const PaymentScreen = ({ history }) => {
     history.push("/shipping");
   }
 
-  const [paymentMethod, setPaymentMethod] = useState("PayPal");
+  const [paymentMethod, setPaymentMethod] = useState("PhonePe");
 
   const dispatch = useDispatch();
 
@@ -24,38 +25,102 @@ const PaymentScreen = ({ history }) => {
   };
 
   return (
-    <FormContainer>
+    <div>
       <CheckoutSteps step1 step2 step3 />
-      <h1>Payment Method</h1>
-      <Form onSubmit={submitHandler}>
-        <Form.Group>
-          <Form.Label as="legend">Select Method</Form.Label>
-          <Col>
-            <Form.Check
-              type="radio"
-              label="PayPal or Credit Card"
-              id="PayPal"
-              name="paymentMethod"
-              value="PayPal"
-              checked
-              onChange={(e) => setPaymentMethod(e.target.value)}
-            ></Form.Check>
-            {/* <Form.Check
-              type='radio'
-              label='Stripe'
-              id='Stripe'
-              name='paymentMethod'
-              value='Stripe'
-              onChange={(e) => setPaymentMethod(e.target.value)}
-            ></Form.Check> */}
-          </Col>
-        </Form.Group>
 
-        <Button type="submit" variant="primary">
-          Continue
-        </Button>
-      </Form>
-    </FormContainer>
+      <div className="split left">
+        <FormContainer>
+          <h1>Payment Method</h1>
+          <Form onSubmit={submitHandler}>
+            <Form.Group>
+              {/* <Form.Label as="legend">Select Method</Form.Label> */}
+              <Col className="Col">
+                <Form.Check
+                  type="radio"
+                  label="PhonePe"
+                  id="PhonePe"
+                  style={{
+                    marginBottom: "10px",
+                    marginLeft: "20px",
+                    fontSize: "20px",
+                  }}
+                  name="paymentMethod"
+                  value="PhonePe"
+                  checked
+                  onChange={(e) => setPaymentMethod(e.target.value)}
+                ></Form.Check>
+
+                <Form.Check
+                  type="radio"
+                  label="PayTM"
+                  id="PayTM"
+                  style={{
+                    marginBottom: "10px",
+                    marginLeft: "20px",
+                    fontSize: "20px",
+                  }}
+                  name="paymentMethod"
+                  value="PayTM"
+                  onChange={(e) => setPaymentMethod(e.target.value)}
+                ></Form.Check>
+
+                <Form.Check
+                  type="radio"
+                  label="GooglePay"
+                  id="GooglePay"
+                  style={{
+                    marginBottom: "10px",
+                    marginLeft: "20px",
+                    fontSize: "20px",
+                  }}
+                  name="paymentMethod"
+                  value="GooglePay"
+                  onChange={(e) => setPaymentMethod(e.target.value)}
+                ></Form.Check>
+
+                <Form.Check
+                  type="radio"
+                  label="CREDIT/DEBIT/ATM CARD"
+                  id="CREDIT/DEBIT/ATM CARD"
+                  style={{
+                    marginBottom: "10px",
+                    marginLeft: "20px",
+                    fontSize: "20px",
+                  }}
+                  name="paymentMethod"
+                  value="CREDIT/DEBIT/ATM CARD"
+                  onChange={(e) => setPaymentMethod(e.target.value)}
+                ></Form.Check>
+
+                <Form.Check
+                  type="radio"
+                  label="NET BANKING"
+                  id="NET BANKING"
+                  style={{
+                    marginBottom: "10px",
+                    marginLeft: "20px",
+                    fontSize: "20px",
+                  }}
+                  name="paymentMethod"
+                  value="NET BANKING"
+                  onChange={(e) => setPaymentMethod(e.target.value)}
+                ></Form.Check>
+              </Col>
+            </Form.Group>
+
+            <Button type="submit" variant="primary">
+              Continue
+            </Button>
+          </Form>
+        </FormContainer>
+      </div>
+
+      <div className="split right">
+        <figure>
+          <img className="payment" src={payment} alt="Payment Method" />
+        </figure>
+      </div>
+    </div>
   );
 };
 
